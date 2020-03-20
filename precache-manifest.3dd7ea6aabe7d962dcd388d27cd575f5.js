487483cdc2b1e58457d39d6b4df44b31085a2e4d
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2020-03-20 13:12:55 +0200

    Updates

---
+++ precache-manifest.3dd7ea6aabe7d962dcd388d27cd575f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17a44a62b4c6ea87eb9c02b1e9e934c",
+    "url": "/test-kanban/index.html"
+  },
+  {
+    "revision": "94e1ed73b6d91ec58a2a",
+    "url": "/test-kanban/static/js/2.1a259cc0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/test-kanban/static/js/2.1a259cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7bbf13b4ffb1fd02eb6",
+    "url": "/test-kanban/static/js/main.3a1c29e8.chunk.js"
+  },
+  {
+    "revision": "8e142306b44c29093f1b",
+    "url": "/test-kanban/static/js/runtime-main.e9f8a531.js"
+  }
+]);
